85061236494d22bcf1dad32b80253d55c0255ff1
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-04-04 11:21:38 -0300

    Usando componente

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,9 @@
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>
+                Fundamentos de Next.js & React
+            </h1>
+        </header>
+    )
+}
